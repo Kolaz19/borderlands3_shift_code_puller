7464dbb69758422ece1ae47beff81a8311deaf1b
Nikolas Bertelshofer <etc.nikolas@gmail.com>
2020-04-20 13:36:06 +0200

    save.csv is no longer needed and icon does not have to be in directory

--- src/htmlpull/ShiftCodePuller.java
+++ src/htmlpull/ShiftCodePuller.java
@@ -2,6 +2,7 @@ package htmlpull;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -13,11 +14,17 @@ import javax.swing.*;
 public class ShiftCodePuller {
 
 	public static void main(String[] args) throws IOException {
-		//IExpress
+		//See, if save.csv exists, otherwise create one
+		File tmpFile = new File("save.csv");
+		if (!tmpFile.exists()) {
+			FileWriter writer = new FileWriter("save.csv",false);
+			writer.close();
+		}
 		
 		int iter = 0;
 		//Only get array size (for later)
 		Scanner scanner = new Scanner (new File("save.csv"));
+
 		scanner.useDelimiter(";");
 		while (scanner.hasNext()) {
 			iter++;
@@ -121,9 +128,13 @@ public class ShiftCodePuller {
 		table.setBounds(500,500,300,400);
 		JScrollPane scrollPane = new JScrollPane(table);
 		
-		
-		frame.setIconImage(ImageIO.read(new File("bl_icon.png")));
-		
+		//Set icon from local or from URL
+		File icon = new File("icon.png");
+		if (icon.exists()) {
+		frame.setIconImage(ImageIO.read(new File("icon.png")));
+		} else {
+		frame.setIconImage(ImageIO.read(new URL("https://www.seekpng.com/png/full/148-1486747_borderlands-symbol-borderlands-symbols-tattoo-ideas-borderlands-vault.png")));
+		}
 		
 		//Configure frame
 		WindowsListen wlisten = new WindowsListen(tmodel);
